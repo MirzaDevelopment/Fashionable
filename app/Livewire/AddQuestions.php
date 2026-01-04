cd239dca5e4ddeb1a79e50cdced90583ad0f7838
MirzaM <mirza.mehagic@hotmail.com>
2026-01-04 19:30:47 +0100

    small work

--- app/Livewire/AddQuestions.php
+++ app/Livewire/AddQuestions.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class AddQuestions extends Component
 {
+
+    public string $userName;
+    private string $email;
+    public string $question;
+    public string $status;
+    
+
     public function render()
     {
         return view('livewire.add-questions');
