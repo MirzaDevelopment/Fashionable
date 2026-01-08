072506d84c763e38d2a9dc723dae97e9e2b28751
MirzaM <mirza.mehagic@hotmail.com>
2026-01-08 19:50:19 +0100

    finished show produts fron test

--- tests/Feature/Livewire/ShowProductsFrontTest.php
+++ tests/Feature/Livewire/ShowProductsFrontTest.php
@@ -2,6 +2,7 @@
 /*
 Livewire tests that test the ShowProductsFront livewire component. The component is responsible for rendering the products on the first page (/shop).
 */
+
 namespace Tests\Feature\Livewire;
 
 use Livewire\Livewire;
@@ -10,6 +11,10 @@ use App\Livewire\AddProduct;
 use App\Livewire\ShowProductsFront;
 use Tests\TestCase;
 use App\Models\Price;
+use App\Models\Image;
+use App\Models\Tag;
+use App\Models\Material;
+use App\Models\Type;
 use App\Models\User;
 use App\Models\Color;
 use App\Models\Size;
@@ -29,34 +34,43 @@ class ShowProductsFrontTest extends TestCase
     /** @test */
     public function test_component_exists_on_the_page()
     {
-  
+
         $response = $this->get('/shop')
             ->assertSeeLivewire(ShowProductsFront::class);
         $response->assertStatus(200);
     }
 
 
-        /** @test */
+    /** @test */
     public function test_renders_front_products_from_database()
     {
-     $typeSelect = "";
-     $genderSelect = [];
-     $tagSelect = [];
-     $search = "";
-
+ 
+        //Creating fake variables
         $fakeProduct = Product::factory()->create(['product_name' => 'Test Name', 'description' => 'Test description', 'total_stock' => 0]);
-        $fakePrice = Price::factory()->create(['product_id' => $fakeProduct->id, "deleted_at"=>null]);
+        Price::factory()->create(['product_id' => $fakeProduct->id, "deleted_at" => null]);
         $fakeColor = Color::factory()->create();
+        $fakeType=Type::factory()->create();
+        $fakeTag=Tag::factory()->create();
+        $fakeMaterial = Material::factory()->create();
+        $fakeImage = Image::factory()->create();
         $fakeSize = Size::factory()->create();
-        $fakeProduct->colorsVariant()->attach($fakeColor->id, [
-            'category_size_id' => $fakeSize->id,
-            'stock_quantity' => 0
+        //Creating pivot tables
+        $fakeProduct->type($fakeType->id);
+        $fakeProduct->materials()->attach($fakeMaterial->id);
+        $fakeProduct->tags()->attach($fakeTag->id);
+        $fakeProduct->colors()->attach($fakeColor->id);
+        $fakeProduct->images()->attach($fakeImage->id, [
+            'category_color_id' => $fakeColor->id,
         ]);
 
-        if (!$typeSelect && !$tagSelect && !$genderSelect) {
-        Livewire::test(ShowProductsFront::class)->set('search', 'Test')
-            ->assertSee('Test Name');
-            
+        $fakeProduct->colorsVariant()->attach($fakeColor->id, [
+            'category_size_id' => $fakeSize->id,
+            'stock_quantity' => 25
+        ]);
+     
+        
+            Livewire::test(ShowProductsFront::class)
+                ->assertSee('Test Name');
+        
     }
-   }
 }
