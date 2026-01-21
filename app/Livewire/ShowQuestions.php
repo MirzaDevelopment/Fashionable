58d44685304e9e4e4a68bfd29c659cd3dd836e43
MirzaM <mirza.mehagic@hotmail.com>
2026-01-21 19:58:01 +0100

    contact form backend work

--- app/Livewire/ShowQuestions.php
+++ app/Livewire/ShowQuestions.php
@@ -1,13 +1,22 @@
 <?php
-
+/*
+This is a livewire backend component used for rendering questions/comments that users send from first page.
+Its a simple component, that has the render method to render the questions mentioned in to the show-questions view.
+*/
 namespace App\Livewire;
-
+use App\Models\Question;
 use Livewire\Component;
 
 class ShowQuestions extends Component
 {
+
+
+
+
     public function render()
     {
-        return view('livewire.show-questions');
+
+        $questions=Question::all();
+        return view('livewire.show-questions', ["questions"=>$questions]);
     }
 }
