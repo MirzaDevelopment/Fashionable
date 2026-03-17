949d7884c0626a66a81722e3bd024cfec84a3542
MirzaM <mirza.mehagic@hotmail.com>
2026-03-17 22:27:54 +0100

    typos and language fix

--- resources/views/edituser.blade.php
+++ resources/views/edituser.blade.php
@@ -36,7 +36,7 @@
                 </select>
             </div>
             <div class="mt-8 flex place-content-between">
-                <a class="ms-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" href="{{ route('users') }}" wire:navigate>Back to users</a>
+                <a class="ms-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" href="{{ route('users') }}" wire:navigate>Natrag korisnicima</a>
 
                 <x-primary-button class="ms-4">
                     {{ __('Promijeni') }}
