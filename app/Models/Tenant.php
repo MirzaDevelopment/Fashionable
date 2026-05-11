a6176175ca6ce66191f55eef2048ebb977720822
MirzaM <mirza.mehagic@hotmail.com>
2026-05-11 19:35:37 +0200

    adding stuff in model

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tenant extends Model
 {
     use HasFactory;
+
+
+// Specify the table name
+    protected $table = 'tenants';
 }
