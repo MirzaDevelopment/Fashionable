3746f88afd91af4cea95b1d37625699c7f2f3a10
MirzaM <mirza.mehagic@hotmail.com>
2026-07-20 18:47:11 +0200

    adding back to admin panel in show questions

--- resources/views/livewire/show-questions.blade.php
+++ resources/views/livewire/show-questions.blade.php
@@ -27,7 +27,7 @@
             @endif
         </section>
         @endforeach
-
+   <a class="col-start-1 lg:col-span-2 lg2:col-start-1 lg2:col-span-1 justify-center inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" href="{{ route('dashboard') }}" wire:navigate>Natrag na ploču</a>
     </div>
     {{ $questions->links() }}
 </div>
