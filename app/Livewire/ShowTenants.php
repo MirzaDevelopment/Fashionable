4381d45f1814a6fdc2d114cf84ebdf1e4bc559f8
MirzaM <mirza.mehagic@hotmail.com>
2026-09-18 21:34:14 +0200

    creating show tenant livewire component

---
+++ app/Livewire/ShowTenants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ShowTenants extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-tenants');
+    }
+}
