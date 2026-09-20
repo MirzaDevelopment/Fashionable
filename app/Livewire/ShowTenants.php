6d719249d0e4aca5765ad18cd49a77624902926b
MirzaM <mirza.mehagic@hotmail.com>
2026-09-20 12:47:33 +0200

    comments nothing too imp.

--- app/Livewire/ShowTenants.php
+++ app/Livewire/ShowTenants.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+This is a livewire backend component used for rendering active tenants depending on the search filters. It is also used for deleting selected tenants. It includes following methods:
+- Search method that updates the frontend component with the results from the $search property. 
+- Helper method to visually distinguish selected rows
+- Method to delete selected tenants
+- Helper method to clear checked rows
+- Pagination is also used
+*/
 namespace App\Livewire;
 
 use Livewire\Component;
--- resources/views/livewire/show-tenants.blade.php
+++ resources/views/livewire/show-tenants.blade.php
@@ -1,3 +1,3 @@
-<div>
+<div><!--Livewire frontend component for tenants search, delete and modify. Similiar like product search-->
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
 </div>
